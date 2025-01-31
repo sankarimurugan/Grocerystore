b64940fcb7e82544ceb238b2e13a34848bf8f8a4
sankarimurugan <sankarim945@gmail.com>
2025-01-31 12:26:26 +0530

    Grocery storesss sankari

--- src/index.js
+++ src/index.js
@@ -4,21 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Router } from "react-router-dom";
-// import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./DataStore/Redux/store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename="/Grocerystore">
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>
-  </Router>
+  <React.StrictMode>
+    <BrowserRouter basename="/Grocerystore">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
