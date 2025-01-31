e0fe556016cb02fd362718b58b95df64ee991a0a
sankarimurugan <sankarim945@gmail.com>
2025-01-31 12:18:20 +0530

    Grocery storesss sankari

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import { BrowserRouter, Router } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./DataStore/Redux/store/store";
 
